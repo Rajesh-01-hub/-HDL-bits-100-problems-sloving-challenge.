f239b2148dbbc67d6858aab663b3542dea07262c
Avula_Rajesh <rajeshep01avula@gmail.com>
2026-02-12 22:38:46 +0530

    Update and rename adder_ Subtrac_structural  manner.v to adder_ Subtrac.v

--- Day_2/adder_ Subtrac.v
+++ Day_2/adder_ Subtrac.v
@@ -1,19 +1,3 @@
-module  fa(input a,b,cin,output sum,carry_out);
-assign sum = a^b^cin;
-assign carry_out = (a&b)|(c&a)|(c&b);
-endmodule
-
-module LS83(input [3:0]a,b,input  cin,output [3:0]sum,output cout);
-wire [4:0]c;
-assign c[0]=cin;
-genvar i;
-generate 
-for(i=0;i<4;i=i+1) begin:add_loop
-fa h1(.a(a[i]),.b(b[i]),.cin(c[i]),.sum(sum[i]),.carry_out(c[i+1]));
-end
- endgenerate
-endmodule
-
 
 module add_sub(input [15:0]a,b,input mode,output [15:0]sum,output carry_out);
 wire [3:0]c;
