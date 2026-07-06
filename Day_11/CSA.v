f8cfc684692e16028b63f225d50be69d4e88176d
Avula Rajesh <rajeshep01avula@gmail.com>
2026-07-06 18:46:34 +0530

    Carry select adder

--- Day_11/CSA.v
+++ Day_11/CSA.v
@@ -5,6 +5,6 @@ wire[3:0]w0,w1;
 wire c0,c1;
 FA_4 g1(.a(a),.b(b),.cin(0),.sum(w0),.cout(c0));
 FA_4 g2(.a(a),.b(b),.cin(2),.sum(w1),.cout(c1));
-
+assign cout = cin ? c1 : c0;
 mux g3(.a({c0,w0}),.b({c1,w1}),.sel(cin),.out({cout,sum}));
 endmodule
